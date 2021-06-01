b6086cbf766c8103aba79f57d6fe36417ac54b45
Andrew McIndoe <amcindoe@powerhrg.com>
2021-06-01 19:20:27 -0400

    Fetch Lab

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
 }
+
+
+
+// function fetchBooks() {
+//   fetch("https://anapioficeandfire.com/api/books")
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(json){
+// //     return renderBooks(json)
+   
+//   })
+  
+// }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
